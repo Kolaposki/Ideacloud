5ee0b85bb1c99555e0a4e5f18025ba4c6de8d94f
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-03-26 01:14:44 +0100

    Fixed font-awesome error while deployinh

--- ideacloud/settings.py
+++ ideacloud/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'q_khv#tm(79c-34qt__@$zsiaxf4iz*z&pu%%j#w=mzi67&vy7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
