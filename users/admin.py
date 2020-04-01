bf98ce73260e752cc13b62a30ef1f62a319414b9
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-01 13:07:56 +0100

    Add import and export functionality to Users admin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from .models import Profile
+from import_export.admin import ImportExportModelAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
 
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(ImportExportModelAdmin):
     pass
     # list_display = ('username', 'email')
 
 
+class MyUserAdmin(ImportExportModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'is_superuser',)
+    pass
+
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
